e784b80468019581bd8140ac68e0d7543ee3d763
shruthiamu14 <shruthiammu470@gmail.com>
2024-05-24 11:41:44 +0530

    first commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+// TypeScript Data Types
+
+let age: number = 30
+let firstName: string = 'John'
+let isAdult: boolean
+
+age = 31
+firstName = 'Jane'
+isAdult = true
+
+
+let planet = 'Earth'
+let moon = 1
+let isAlive = true
+
+planet = 'Mars'
+moon = 2
+isAlive = false
+
+//null and undefined
+let something = null
+let otherThing = undefined
+
+
+
